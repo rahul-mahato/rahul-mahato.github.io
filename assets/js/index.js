a5a17c1558d3beab85477553f468eb2bc6573eab
Rahul Mahato <1705256@kiit.ac.in>
2020-03-28 19:07:27 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+
+
+setInterval(() => {
+    console.log("check");
+    let n1 = document.querySelector('.name-intro1');
+    let n2 = document.querySelector('.name-intro2');
+    
+    if (n2.classList.contains('d-none')) {
+        n2.classList.add('fadeIn');
+        n2.classList.remove('d-none');
+        n1.classList.add('d-none');
+        n2.classList.remove('d-none');
+    }
+    else{
+        n1.classList.add('fadeIn');
+        n1.classList.remove('d-none');
+        n2.classList.add('d-none');
+        n1.classList.remove('d-none');
+    }
+}, 3000);
+
+
+let servicesSection  = document.querySelector('.services');
+// let home = document.querySelector('.home');
+// let headerSection = document.querySelector('.intro');
+let aboutSection  = document.querySelector('.about');
+let footer = document.querySelector('.footer');
+// navlinks
+let about = document.getElementById('about');
+let services = document.getElementById('services');
+let home = document.getElementById('home');
+
+
+window.addEventListener('scroll',(e)=>{
+    
+    if(window.scrollY > aboutSection.offsetHeight ){
+        document.getElementById('navbarNavAltMarkup').classList.add('bg-dark2')
+    }
+    else{
+        document.getElementById('navbarNavAltMarkup').classList.remove('bg-dark2')
+    }
+
+    // aboutactive
+    if(window.scrollY  > 894.4000244140625        && window.scrollY < servicesSection.offsetTop){
+        about.classList.add('active');
+        home.classList.remove('active');
+        services.classList.remove('active');
+        console.log("act")        
+    }
+    // serviceactive
+    else if(window.scrollY > servicesSection.offsetTop && window.scrollY < footer.offsetTop){
+        services.classList.add('active');
+        home.classList.remove('active');
+        about.classList.remove('active');
+        console.log("act")        
+    }
+    else{
+        services.classList.remove('active');
+        home.classList.add('active');
+        about.classList.remove('active');
+    }
+
+})
+// if(window.scrollY === about.offsetHeight )
